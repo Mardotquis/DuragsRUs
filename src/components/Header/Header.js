89fb3e7962598176d5045931e7fc1c341f019fc2
Marquis Mitchell <marquis0403@gmail.com>
2018-09-17 23:54:22 -0400

    added fixed header and updated small iphone and ipad media queries

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <nav className="header">
-      <NavLink to="/" className="header__name" activeClassName="active" exact>
+      <NavLink to="/" className="header__name" activeStyle="active" exact>
         DuragsRUs
       </NavLink>
       <NavLink to="/">
@@ -13,13 +13,13 @@ const Header = () => {
       <ul className="header__extralinks">
         <li>
           {" "}
-          <NavLink to="/products" activeClassName="active">
+          <NavLink to="/products" activeClassName="activeLink">
             Products
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="/contact" activeClassName="active">
+          <NavLink to="/contact" activeClassName="activeLink">
             Contact
           </NavLink>
         </li>
