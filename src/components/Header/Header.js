f142f77f52d0a7be3ba687f83a93adb1aa0512e6
Marquis Mitchell <marquis0403@gmail.com>
2018-10-02 20:31:05 -0400

    started mobile hamburger icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,61 +3,114 @@ import { NavLink } from "react-router-dom";
 import auth0Client from "../../Auth/Auth";
 
 const Header = () => {
+  const toggleMenuIcon = () => {
+    const menuIcon = document.querySelector('.menu__icon');
+    const biggerMenu = document.querySelector('.header__hidden_links');
+    biggerMenu.style.display = "flex";
+  }
   return (
-    <nav className="header">
-      <NavLink
-        to="/"
-        className="header__name"
-        activeClassName="activeLink"
-        exact
-      >
-        DuragsRUs
+    <header>
+      <nav className="header">
+        <NavLink
+          to="/"
+          className="header__name"
+          activeClassName="activeLink"
+          exact
+        >
+          DuragsRUs
       </NavLink>
-      <NavLink to="/">
-        <div className="header__icon" />
-      </NavLink>
+        <NavLink to="/">
+          <div className="header__icon" />
+        </NavLink>
 
-      <ul className="header__extralinks">
-        <li>
-          <NavLink to="/products" activeClassName="activeLink">
-            Products
+        <ul className="header__extralinks">
+          <li>
+            <NavLink to="/products" activeClassName="activeLink">
+              Products
           </NavLink>
-        </li>
+          </li>
 
-        <li>
-          <NavLink to="/contact" activeClassName="activeLink">
-            Contact
+          <li>
+            <NavLink to="/contact" activeClassName="activeLink">
+              Contact
           </NavLink>
-        </li>
-        {/* <li>
+          </li>
+          {/* <li>
           <NavLink to="/admin" >
             <div className="accountToggle"></div>
           </NavLink>
         </li> */}
 
-        <div className="auth0">
-          {auth0Client.isAuthenticated() ? (
-            <div>
-              <li>
-                <NavLink to="/admin" className="accountToggle"> </NavLink>
-              </li>
-              <li><NavLink to="/admin/contact-info" className="contactToggle"></NavLink></li>
-            </div>
+          <div className="auth0">
+            {auth0Client.isAuthenticated() ? (
+              <div>
+                <li>
+                  <NavLink to="/admin" className="accountToggle"> </NavLink>
+                </li>
+                <li><NavLink to="/admin/contact-info" className="contactToggle"></NavLink></li>
+              </div>
 
 
 
-          ) : null}
+            ) : null}
 
-          {/* AUTH0 */}
-          {!auth0Client.isAuthenticated() && (
-            <li className="header__auth" onClick={auth0Client.signIn} >Log In</li>
-          )}
-          {auth0Client.isAuthenticated() && (
-            <li className="header__auth" onClick={auth0Client.signOut} >Log Out</li>
-          )}
-        </div>
-      </ul>
-    </nav >
+            {/* AUTH0 */}
+            {!auth0Client.isAuthenticated() && (
+              <li className="header__auth" onClick={auth0Client.signIn} >Log In</li>
+            )}
+            {auth0Client.isAuthenticated() && (
+              <li className="header__auth" onClick={auth0Client.signOut} >Log Out</li>
+            )}
+          </div>
+
+        </ul>
+        <ul className="header__hidden_links">
+          <li>
+            <NavLink to="/products" activeClassName="activeLink">
+              Products
+          </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/contact" activeClassName="activeLink">
+              Contact
+          </NavLink>
+          </li>
+          {/* <li>
+          <NavLink to="/admin" >
+            <div className="accountToggle"></div>
+          </NavLink>
+        </li> */}
+
+          <div className="auth0">
+            {auth0Client.isAuthenticated() ? (
+              <div>
+                <li>
+                  <NavLink to="/admin" className="accountToggle"> </NavLink>
+                </li>
+                <li><NavLink to="/admin/contact-info" className="contactToggle"></NavLink></li>
+              </div>
+
+
+
+            ) : null}
+
+            {/* AUTH0 */}
+            {!auth0Client.isAuthenticated() && (
+              <li className="header__auth" onClick={auth0Client.signIn} >Log In</li>
+            )}
+            {auth0Client.isAuthenticated() && (
+              <li className="header__auth" onClick={auth0Client.signOut} >Log Out</li>
+            )}
+          </div>
+
+        </ul>
+
+      </nav >
+      <div className="header__overlay">
+        <div className="menu__icon" onClick={toggleMenuIcon} />
+      </div>
+    </header>
   );
 };
 
