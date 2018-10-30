5b909643a8168b1229742dc349011d05a1389edc
Marquis Mitchell <marquis0403@gmail.com>
2018-10-30 11:50:31 -0400

    removed testing commits

--- src/SecuredRoute/SecuredRoute.js
+++ src/SecuredRoute/SecuredRoute.js
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import { Route } from 'react-router-dom';
-// import auth0Client from '../Auth/Auth';
-// // import ContactAdmin from '../components/ProductManager/ContactAdmin/ContactAdmin';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import auth0Client from '../Auth/Auth';
+// import ContactAdmin from '../components/ProductManager/ContactAdmin/ContactAdmin';
 
-// function SecuredRoute(props) {
-//     const { component: Component, path } = props;
-//     // const { component: ContactAdmin, path } = props;
-//     return (
-//         <Route path={path} render={() => {
-//             if (!auth0Client.isAuthenticated()) {
-//                 auth0Client.signIn();
-//                 return <div></div>;
-//             }
-//             return <Component contactsData={props.contactsData} products={props.products} />
+function SecuredRoute(props) {
+    const { component: Component, path } = props;
+    // const { component: ContactAdmin, path } = props;
+    return (
+        <Route path={path} render={() => {
+            if (!auth0Client.isAuthenticated()) {
+                auth0Client.signIn();
+                return <div></div>;
+            }
+            return <Component contactsData={props.contactsData} products={props.products} />
 
-//         }} />
-//     );
-// }
+        }} />
+    );
+}
 
-// export default SecuredRoute;
+export default SecuredRoute;
